8dc475d441f69532d64a8635350be3e0af6ceeae
ugurcansonm <ugurcansonmezoglu@gmail.com>
2023-10-17 13:39:00 +0300

    Projects Files
    A beginner-level game designed using Unity, 'Flappy Bird'.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    public int score;
+    public Text ScoreText;
+    void Start()
+    {
+        score = 0;
+        ScoreText.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateScore()
+    {
+        score++;
+        ScoreText.text = score.ToString();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
